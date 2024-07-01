00cef38fe2eceb0779f70fe3d2e57ca6dae0ee56
Minna Palm <mppalm@utu.fi>
2024-07-01 15:19:46 +0300

    Uusi rakenne/siirretty koodia from: index.js to: app.js

--- index.js
+++ index.js
@@ -1,74 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-let observations = [
-    {
-      species: "Auroraperhonen",
-      amount: "6",
-      place: "Aurajoki, Turku",
-      date: "5.6.2024"
-    },
-    {
-        species: "Alppiruusu",
-        amount: "6",
-        place: "Ruissalo, Turku",
-        date: "1.5.2024"
-    },
-    {
-        species: "Krookus",
-        amount: "2",
-        place: "Nummi, Turku",
-        date: "1.6.2024"
-    }
-  ]
-
-app.get('/api/observations', (req, res) => {
-    res.json(observations)
-})
-
-app.get('/api/persons/:id', (request, response) =>{
-  const id = Number(request.params.id)
-  const observation = observations.find(obs => obs.id === id)
-
-  if (observation) {
-    response.json(observation)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/observations/:id', (request, response) => {
-  const id = Number(request.params.id)
-  observations = observations.filter(obs => obs.id !== id)
-  response.status(204).end()
-})
-
-app.post('/api/observations', (request, response) => {
-  const body = request.body
-
-  if (body.species === undefined || body.amount === undefined || body.place === undefined || body.date === undefined) {
-    return response.status(400).json({
-      error: 'Laji, lukumäärä, paikka tai aika puuttuu.'
-    })
-  }
-
-  const observation = new Observation ({
-    species: body.species,
-    amount: body.amount,
-    place: body.place,
-    date: body.date,
-  })
-
-  observation.save().then(savedObs => {
-    response.json(savedObs)
-  })
-
-
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
